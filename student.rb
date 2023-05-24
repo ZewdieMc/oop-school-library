72e31bbe5fcecafd70b612187b7b003d5e920385
ZewdieMc <zewdiehabtie26@gmail.com>
2023-05-24 23:35:31 +0300

    Modify classroom setter

--- student.rb
+++ student.rb
@@ -1,11 +1,14 @@
 require_relative 'person'
 
 class Student < Person
-  attri_accessor :classroom
-  def initialize(age:, classroom:, parent_permission: true, name: 'Unknown')
+  attr_reader :classroom
+  def initialize(age:, parent_permission: true, name: 'Unknown')
     super(age: age, parent_permission: parent_permission, name: name)
+  end
+
+  def classroom=(classroom)
     @classroom = classroom
-    classroom.students << self
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   def play_hooky

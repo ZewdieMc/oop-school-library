f19810c602190e0104c4804147c0d23f32ff4fd9
ZewdieMc <zewdiehabtie26@gmail.com>
2023-05-24 23:25:54 +0300

    Add classroom to student

--- student.rb
+++ student.rb
@@ -1,9 +1,11 @@
 require_relative 'person'
 
 class Student < Person
+  attri_accessor :classroom
   def initialize(age:, classroom:, parent_permission: true, name: 'Unknown')
     super(age: age, parent_permission: parent_permission, name: name)
     @classroom = classroom
+    classroom.students << self
   end
 
   def play_hooky

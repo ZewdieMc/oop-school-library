f5fb21e94e395b16f5e385216386bc8d06296dd9
ZewdieMc <zewdiehabtie26@gmail.com>
2023-05-24 23:36:32 +0300

    Add linebreak after attr_reader

--- student.rb
+++ student.rb
@@ -2,6 +2,7 @@ require_relative 'person'
 
 class Student < Person
   attr_reader :classroom
+
   def initialize(age:, parent_permission: true, name: 'Unknown')
     super(age: age, parent_permission: parent_permission, name: name)
   end

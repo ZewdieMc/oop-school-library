a27bb23be4af18e0ae2b452225576e4f0f71e795
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-05-25 12:07:59 +0300

    Update rental.rb

--- rental.rb
+++ rental.rb
@@ -4,9 +4,9 @@ class Rental
     @date = date
 
     @book = book
-    @book.rentals << self
+    book.rentals << self
 
     @person = person
-    @person.rentals << self
+    person.rentals << self
   end
 end

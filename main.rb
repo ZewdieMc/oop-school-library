257e97a46488a12c345456f5dae083db6439d7f3
ZewdieMc <zewdiehabtie26@gmail.com>
2023-05-27 16:19:46 +0300

    Update options text

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ def options
   puts '3 - Create a person.'
   puts '4 - Create a book.'
   puts '5 - Create a rental.'
-  puts '6 - Create List all rentals for a given person id.'
+  puts '6 - List all rentals for a given person id.'
   puts '7 - Exit.'
 end
 
